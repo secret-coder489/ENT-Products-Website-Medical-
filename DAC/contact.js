03620e767ab0f374a0f95d42eae8c736bc32bd6d
Pragati <pragatitiwari678@gmail.com>
2021-05-31 01:31:30 +0530

    second commit

---
+++ DAC/contact.js
@@ -0,0 +1,36 @@
+var form = document.getElementById("my-form");
+var status = document.getElementById("status");
+
+
+function success(){
+    form.reset();
+    status.innerHTML = "Thanks For submitting Form!";
+}
+function error(){
+    status.innerHTML="Oops! There was a problem.";
+}
+    
+    //handle function for sending on AJAX request
+    form.addEventListener("submit",function (ev){
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+
+
+    ///helper function for sending on AJAX request
+
+    function ajax(method, url, data, success, error){
+        var xhr =new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange=function(){
+            if(xhr.readyState !== XMLHttpRequest.DONE) return;
+            if(xhr.status === 200){
+                success(xhr.response, xhr.responseType);
+            }else{
+                error(xhr.status, xhr.response, xhr.responseType);
+            }
+        };
+        xhr.send(data);
+    }
